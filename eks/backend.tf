05656928b7d0b58b4ea1329264781555db58a697
ashishdubey123 <116423797+ashishdubey123@users.noreply.github.com>
2025-06-08 16:33:47 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -6,7 +6,13 @@ terraform {
       version = "~> 5.49.0"
     }
   }
-
+  backend "s3" {
+    bucket         = "dev-ashish-tf-bucket"
+    region         = "us-east-1"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
